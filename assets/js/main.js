05986838428d49f9a4f2dd60fea8ec57196f2cde
mukarukuria <timothy.kuria@strathmore.edu>
2022-06-25 20:42:20 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-// *** Write your code here***
+let buttons = document.querySelectorAll(".numButtons");
+let history = document.querySelector("#history");
+let ans = document.querySelector("#displayResult");
+let clear = document.querySelector(".btn-clear");
+let equal = document.querySelector(".red")
+
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(e) {
+        let values = e.target.dataset.num;
+        history.value += values;
+    })
+})
+ clear.addEventListener('click', function(e) {
+    history.value ="";   
+    ans.value="";
+ })
+ equal.addEventListener('click', function(e){
+    let answer = eval(history.value);
+    ans.value = answer;
+ })
